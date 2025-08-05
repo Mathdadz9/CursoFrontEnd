f4e35d994571574f1ada2d26be187a39f48e5894
Mathdadz9 <matheuscrippacrippa@gmail.com>
2025-08-05 11:29:17 -0300

    05.08.25

---
+++ FrameWorks/Angular16/projeto-auth/src/app/guardas/auth.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../servicos/auth.service';
+
+export class AuthGuard implements CanActivateFn  {
+
+  constructor (private authService: AuthService, private router: Router){}
+
+   canActivate(): boolean { 
+     if(this.authService.estaAutenticado()) { 
+       return true; 
+     }
+      this.router.navigate(['/login']) 
+      return false;
+   }
+}
+
+
